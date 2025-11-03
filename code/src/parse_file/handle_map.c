60026975a28fe6a409502e9e8583c041a2f72550
Ivan <ivan.pavlov1203@gmail.com>
2025-11-03 15:33:54 +0100

    instal framework for mac

--- code/src/parse_file/handle_map.c
+++ code/src/parse_file/handle_map.c
@@ -15,12 +15,12 @@
 int	count_width(t_game **game)
 {
 	int 	counter;
-	int		i;
+	// int		i;
 	t_list	*tmp;
 	int		len;
 
 	counter = 0;
-	i = 0;
+	// i = 0;
 	tmp = (*game)->copy_map; 
 	while (tmp != NULL)
 	{
@@ -37,7 +37,7 @@ int	count_height(t_game **game)
 	return (ft_lstsize((*game)->copy_map));
 }
 
-int	find_start_position(t_game **game, int i)
+int	find_start_position(t_game **game, int i)`
 {
 	int	j;
 
